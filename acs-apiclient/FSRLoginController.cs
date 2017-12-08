26134016a154d67424f31db5aa2f00bc5a4a5c05
ahmad <ahmad.husseini@foresee.com>
2017-12-08 09:18:22 -0800

    MOBILEAPPS-1750: Added `/idps` endpoint to determine sign on flow

---
+++ acs-apiclient/FSRLoginController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using AcsApi.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+
+namespace AcsApi
+{
+    public class FSRLoginController
+    {
+        FSRLoginConfiguration configuration;
+
+        FSRLoginDelegate loginDelegate;
+
+        public FSRLoginController(FSRLoginConfiguration configuration, FSRLoginDelegate loginDelegate)
+        {
+            this.configuration = configuration;
+            this.loginDelegate = loginDelegate;
+        }
+
+        public void InitializeIdentity(string username)
+        {
+            var client = new RestClient(configuration.BaseUrl);
+            var request = new RestRequest("/idps", Method.GET);
+
+            request.AddHeader("Accept", "application/json");
+
+            request.AddParameter("username", username);
+            request.AddParameter("consumer", configuration.Consumer.ToParameterString());
+
+            var response = client.Execute(request);
+            var results = JsonConvert.DeserializeObject<JObject>(response.Content);
+
+            var identityProviders = JsonConvert
+                .DeserializeObject<List<IdentityProvider>>(results["identityProviders"].ToString());
+            
+            if (identityProviders.Count == 0)
+            {
+                loginDelegate.ShouldBeginPasswordFlow();
+            }
+            else if (identityProviders.Count == 1)
+            {
+                loginDelegate.ShouldBeginExternalFlow();
+            }
+            else
+            {
+                loginDelegate.ShowIdentityProviderSelector(identityProviders);
+            }
+
+        }
+    }
+}
