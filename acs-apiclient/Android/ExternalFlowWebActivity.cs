bcf72b7e61f033949491c3eb4d95f5e35794bf21
swuforesee <31418202+swuforesee@users.noreply.github.com>
2017-12-22 16:10:55 -0800

    MOBILEAPPS-1997: initial setup android project

---
+++ acs-apiclient/Android/ExternalFlowWebActivity.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace acs_apiclient.Android
+{
+    [Activity(Label = "ExternalFlowWebActivity")]
+    public class ExternalFlowWebActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            //TODO add the webview here
+            
+            //TODO add reference to LoginController
+        }
+    }
+}
---
+++ acs-apiclient/Android/LoginController.Android.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace AcsApi
+{
+	public partial class LoginController
+	{
+        public override void BeginLoginFlow<T>(T context, Models.IdentityProvider identityProvider)
+        {
+            
+        }
+        
+        protected override void RunOnMainThread(Action action)
+        {
+            throw new NotImplementedException();
+        }
+        
+        protected override void RunOnBackgroundThread(Action action)
+        {
+            throw new NotImplementedException();
+        }
+	}
+}
