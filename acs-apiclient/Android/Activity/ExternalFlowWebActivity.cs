588539802a105108c4e7aace83705c739a33412f
swuforesee <31418202+swuforesee@users.noreply.github.com>
2017-12-26 14:41:27 -0800

    MOBILEAPPS-1997: setup listeners for close and sync button. Setup title.

--- acs-apiclient/Android/Activity/ExternalFlowWebActivity.cs
+++ acs-apiclient/Android/Activity/ExternalFlowWebActivity.cs
@@ -1,8 +1,11 @@
 ﻿
+using System;
 using Android.App;
 using Android.OS;
 using Android.Views;
 using Android.Webkit;
+using acs_apiclient.Android.CustomViews;
+using Android.Widget;
 
 namespace acs_apiclient.Android
 {
@@ -18,7 +21,35 @@ namespace acs_apiclient.Android
             base.OnCreate(savedInstanceState);
             this.Window.AddFlags(WindowManagerFlags.Fullscreen);
             this.SetContentView(Resource.Layout.activity_externalFlowWeb);
+            String title = savedInstanceState.GetString(UrlParamKey);
+            SetupTitle(title);
+            SetupCloseButton();
+            SetupSyncButton();
             SetupWebView();
+        }
+
+        private void SetupTitle(string newTitle)
+        {
+            TextView title = FindViewById<TextView>(Resource.Id.textview_toolbarContainer_title);
+            title.Text = newTitle;
+        }
+
+        private void SetupSyncButton()
+        {
+            EasyTintImageButton syncButton = FindViewById<EasyTintImageButton>(Resource.Id.easyTintImageButton_titlebarWithExitAndRefereshButtons_sync);
+            syncButton.Click += (object sender, EventArgs e) => 
+            {
+                //TODO implement behaviour after webview is setup
+            };
+        }
+
+        private void SetupCloseButton()
+        {
+            EasyTintImageButton closeButton = FindViewById<EasyTintImageButton>(Resource.Id.easyTintImageButton_titlebarWithExitAndRefereshButtons_sync);
+            closeButton.Click += (object sender, EventArgs e) => 
+            {
+                Finish();
+            };
         }
 
         private void SetupWebView()
