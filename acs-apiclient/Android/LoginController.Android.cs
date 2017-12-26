5b5144e3a0503b3809b079a3e7d775ade4af04a6
swuforesee <31418202+swuforesee@users.noreply.github.com>
2017-12-26 10:13:35 -0800

    MOBILEAPPS-1997: clean up code.

--- acs-apiclient/Android/LoginController.Android.cs
+++ acs-apiclient/Android/LoginController.Android.cs
@@ -9,37 +9,6 @@ namespace AcsApi
 {
 	public partial class LoginController
 	{
-        //        /// <summary>
-        ///// Initiates the login flow from the specified UIViewController if SSO is enabled. Otherwise, 
-        ///// the LoginController will inform its delegate to show the password field for the normal flow.
-        ///// </summary>
-        ///// <param name="context">
-        ///// The UIViewController which will handle presenting the ExternalFlowWebViewController
-        ///// </param>
-        ///// <param name="identityProvider">The chosen identity provider.</param>
-        //public override void BeginLoginFlow<T>(T context, IdentityProvider identityProvider)
-        //{
-        //    if (!(context is UIViewController))
-        //    {
-        //        throw new ArgumentException(
-        //            $"{ typeof(T) } is not implemented, please make sure you are using a UIViewController for the " +
-        //            "context parameter."
-        //        );
-        //    }
-        //    var loginFlowContext = context as UIViewController;
-        //    selectedIdentityProvider = identityProvider;
-        //    if (identityProvider.IsSSOEnabled)
-        //    {
-        //        var externalLoginViewController = new ExternalFlowWebViewController(identityProvider.UrlString, this);
-        //        var navigationController = new UINavigationController(externalLoginViewController);
-        //        loginFlowContext.PresentViewController(navigationController, true, null);
-        //    }
-        //    else 
-        //    {
-        //        loginDelegate.ShouldBeginPasswordFlow();
-        //    }
-        //}
-        
         public override void BeginLoginFlow<T>(T context, Models.IdentityProvider identityProvider)
         {
             if (!(context is Context))
@@ -49,9 +18,10 @@ namespace AcsApi
                     "context parameter."
                 );
             }
-            var loginFlowContext = context as Context;
             
+            var loginFlowContext = context as Context;
             selectedIdentityProvider = identityProvider;
+            
             if (identityProvider.IsSSOEnabled)
             {
                 var webActivity = new Intent (loginFlowContext, typeof(ExternalFlowWebActivity));
@@ -72,7 +42,7 @@ namespace AcsApi
         
         protected override void RunOnBackgroundThread(Action action)
         {
-            ThreadPool.QueueUserWorkItem(o => action());
+            ThreadPool.QueueUserWorkItem(calback => action());
         }
 	}
 }
