c311db20c8129fb794b31d75286008defbbd07d8
swuforesee <31418202+swuforesee@users.noreply.github.com>
2018-03-13 12:16:56 -0700

    MOBILEAPPS-2170: add android unit tests.

---
+++ AndroidUnitTests/TestsSample.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using AcsApi;
+using NUnit.Framework;
+
+namespace AndroidUnitTests
+{
+    [TestFixture]
+    public class TestsSample
+    {
+        public const string ConsumerKey = "***REMOVED***";
+        public const string ConsumerSecret = "***REMOVED***";
+        
+        [SetUp]
+        public void Setup() { }
+
+
+        [TearDown]
+        public void Tear() { }
+
+        [Test]
+        public void AuthenticateOnPROD()
+        {
+            bool hasToken = HasToken("***REMOVED***", "***REMOVED***", "https://services-edge.foresee.com", "https://portal2.foreseeresults.com");
+            Assert.True(hasToken);
+        }
+
+        [Test]
+        public void AuthenticateOnDEV()
+        {
+            bool hasToken = HasToken("***REMOVED***", "***REMOVED***", "https://services-edge-dev.foresee.com", "https://portal2.foreseeresults.com");
+            Assert.True(hasToken);
+        }
+        
+        static bool HasToken(string username, string password, string authenticationBaseUrl, string serviceBaseUrl)
+        {
+            var clientConfig = new AcsApiClientConfig(ConsumerKey, ConsumerSecret, username, password, authenticationBaseUrl, serviceBaseUrl);
+            var foreseeClient = new AcsApiClient(clientConfig);
+            bool hasToken = HasOAuthToken(foreseeClient, serviceBaseUrl);
+            Console.WriteLine($"{authenticationBaseUrl} hasOAuthToken: {hasToken}");
+            return hasToken;
+        }
+
+        static bool HasOAuthToken(IAcsApiClient client, string serviceBaseUrl)
+        {
+            try
+            {
+                string url = $"{serviceBaseUrl}" + "/currentUser/";
+                var uri = new Uri(url);
+                var oauthtoken = client.GetAuthHeadersForRequestByType(url, "GET");
+                Console.WriteLine($"token={oauthtoken}");
+                return !string.IsNullOrEmpty(oauthtoken);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine( $"{serviceBaseUrl}" + e);
+                return false;
+            }
+        }
+    }
+}
